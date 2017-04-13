080e0c44cb712600c156bffc3a5cc777c6780a20
ashish <ashish.mlr.123@gmail.com>
2017-04-13 16:38:55 +0530

    changing sqlite3 backend

--- djangobackend/djangobackend/settings.py
+++ djangobackend/djangobackend/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'djangobackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'stocks',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'POST': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
