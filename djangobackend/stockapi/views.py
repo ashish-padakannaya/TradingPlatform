0fc9b6138d5417afbc28a3c2aa5018dbf28b5980
ashish <ashish.mlr.123@gmail.com>
2017-04-14 15:11:28 +0530

    pushing fix for boring, exciting logic

--- djangobackend/stockapi/views.py
+++ djangobackend/stockapi/views.py
@@ -113,22 +113,31 @@ class getPointer(APIView):
 			else:
 				color = 'red'
 
-			stick_length = close - open
+			if close > open:
+				body_length = close - open
+			if open > close:
+				body_length = open - close
 
-			upper_shadow_length = 0
-			lower_shadow_lenght = 0
+			upper_stick_length = 0
+			lower_stick_length = 0
 
 			if high > close:
-				upper_shadow_length = high - close
+				if color is 'green':
+					upper_stick_length = high - close
+				else:
+					upper_stick_length = high - open
 			if low < open:
-				lower_shadow_lenght = open - low
+				if color is 'green':
+					lower_stick_length = open - low
+				else:
+					lower_stick_length = close - open
 
-			shadow_length = upper_shadow_length + lower_shadow_lenght
+			stick_length = upper_stick_length + lower_stick_length
 
-			if shadow_length >= stick_length:
-				nature = 'exciting'
-			else:
+			if stick_length >= body_length:
 				nature = 'boring'
+			else:
+				nature = 'exciting'
 
 			data.set_value(index, 'color', color)
 			data.set_value(index, 'nature', nature)
