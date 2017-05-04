cfd2a616962c884ecb7dbce6c5787634f7bef997
ashish <ashish.mlr.123@gmail.com>
2017-05-04 23:18:31 +0530

    pushing changes for final app
    ~modularize functions
    ~bug fixes

--- stockapi/views.py
+++ stockapi/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from rest_framework import generics, status
-from stockapi.models import stockData
+#from stockapi.models import stockData
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from datetime import datetime, timedelta
@@ -12,7 +12,8 @@ import os
 
 
 import quandl
-quandl.ApiConfig.api_key = 'VHeUNLxuAngRYDgtjD9X'
+# quandl.ApiConfig.api_key = 'VHeUNLxuAngRYDgtjD9X'
+quandl.ApiConfig.api_key = 'GX3otZafamJ5s9zfz7nR'
 
 
 def getNatureAndColor(row):
@@ -35,16 +36,12 @@ def getNatureAndColor(row):
 	upper_stick_length = 0
 	lower_stick_length = 0
 
-	if high > close:
-		if color is 'green':
-			upper_stick_length = high - close
-		else:
-			upper_stick_length = high - open
-	if low < open:
-		if color is 'green':
-			lower_stick_length = open - low
-		else:
-			lower_stick_length = close - low
+	if color is 'green':
+		upper_stick_length = high - close
+		lower_stick_length = open - low
+	else:
+		upper_stick_length = high - open
+		lower_stick_length = close - low
 
 	stick_length = upper_stick_length + lower_stick_length
 
@@ -54,20 +51,6 @@ def getNatureAndColor(row):
 		nature = 'exciting'
 
 	return nature, color
-
-
-# Create your views here.
-# class authenticateUser(generics.ListCreateAPIView):
-# 	def get(self, request, format=None):
-# 		try:
-# 			username = request.query_params['username']
-# 			password = request.query_params['password']
-# 		except Exception:
-# 			return Response('Username/Password missing', status=status.HTTP_400_BAD_REQUEST)
-# 		if userData.objects.filter(username=username, password=password):
-# 			return Response(True)
-
-# 		return Response(False)
 
 
 class getAllStocks(generics.ListCreateAPIView):
@@ -134,6 +117,9 @@ class getStock(APIView):
 class getPointer(APIView):
 	def post(self, request, format=None):
 		print request.data
+		multiplier = 2
+		if 'multiplier' in request.data:
+			multiplier = float(request.data['multiplier'])
 		if 'ticker' in request.data:
 			ticker = request.data['ticker']
 		else:
@@ -141,11 +127,9 @@ class getPointer(APIView):
 
 		table_code = 'NSE/' + ticker
 		try:
-			start_date = datetime.strftime(datetime.now() - timedelta(days=365), '%Y-%m-%d')
-			end_date = datetime.strftime(datetime.now(), '%Y-%m-%d')
-			print start_date
-			print end_date
-			data = quandl.get(table_code, start_date=start_date, end_date=end_date)
+			startDate = datetime.strftime(datetime.now() - timedelta(days=365), '%Y-%m-%d')
+			endDate = datetime.strftime(datetime.now(), '%Y-%m-%d')
+			data = quandl.get(table_code, start_date=startDate, end_date=endDate)
 		except Exception:
 			return Response('Incorrect ticker', status=status.HTTP_400_BAD_REQUEST)
 
@@ -163,6 +147,10 @@ class getPointer(APIView):
 
 		for index, row in data.iterrows():
 			nature, color = getNatureAndColor(row)
+
+			# if lowAfterEntry is None or row.Low < lowAfterEntry:
+			# 	lowAfterEntry = row.Low
+
 			data.set_value(index, 'color', color)
 			data.set_value(index, 'nature', nature)
 
@@ -193,7 +181,8 @@ class getPointer(APIView):
 		if not P1 and not P2 and not P3:
 			return Response({'entry': None, 'stopLoss': None, 'target': None})
 
-		print data[:index + 1]
+		# if index == len(data) - 1:
+		# 	endOfDataHit = True
 
 		# print P1index
 		# print data.loc[[P1index]]
@@ -206,32 +195,24 @@ class getPointer(APIView):
 		# pointers found, now to find the data
 		entry = 0
 		entry_at_index = 0
-		#entry_index = 0
 		for index, row in data[P2index:P3index].iterrows():
 			if row.nature == 'boring':
 				# entry_at_index = row.High
 				entry_at_index = max(row.Open, row.Close)
 				if entry_at_index > entry:
 					entry = float(entry_at_index)
-					#entry_index = index
-
-		# print 'entry is ' + str(entry)
-		# print data.loc[[entry_index]]
 
 		stopLoss = None
-		stopLoss_at_index = 0
+		stopLossAtIndex = 0
 		#stopLossIndex = 0
 		for index, row in data[P1index:P3index].iterrows():
 			# if (row.color == 'green' and row.nature == 'exciting') or (row.nature == 'boring'):
-			stopLoss_at_index = row.Low
-			if stopLoss is None or stopLoss_at_index < stopLoss:
-				stopLoss = float(stopLoss_at_index)
-					#stopLossIndex = index
-		# print data.loc[[stopLossIndex]]
+			stopLossAtIndex = row.Low
+			if stopLoss is None or stopLossAtIndex < stopLoss:
+				stopLoss = float(stopLossAtIndex)
+			# print stopLoss
 
-		# print stopLoss
-
-		target = ((entry - stopLoss) * 2) + entry
+		target = ((entry - stopLoss) * multiplier) + entry
 
 		data_to_return = {'entry': entry, 'stopLoss': stopLoss, 'target': target}
 
