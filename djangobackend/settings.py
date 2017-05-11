2179a19f51ff04445461e4babb8884b1e918e71f
ashish <ashish.mlr.123@gmail.com>
2017-05-11 17:17:35 +0530

    testing expiration

--- djangobackend/settings.py
+++ djangobackend/settings.py
@@ -144,7 +144,7 @@ JWT_AUTH = {
     'JWT_VERIFY': True,
     'JWT_VERIFY_EXPIRATION': True,
     'JWT_LEEWAY': 0,
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=50000),
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=50),
     'JWT_AUDIENCE': None,
     'JWT_ISSUER': None,
 
