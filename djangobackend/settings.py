baa758e047e4f1d9d05966bd83bbe8c7d5c06cc1
ashish <ashish.mlr.123@gmail.com>
2017-05-11 17:28:46 +0530

    changing expiration to 0.5 hours

--- djangobackend/settings.py
+++ djangobackend/settings.py
@@ -144,7 +144,7 @@ JWT_AUTH = {
     'JWT_VERIFY': True,
     'JWT_VERIFY_EXPIRATION': True,
     'JWT_LEEWAY': 0,
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=50),
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(hours=0.5),
     'JWT_AUDIENCE': None,
     'JWT_ISSUER': None,
 
