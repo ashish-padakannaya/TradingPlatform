94502b2e69e8eada4e1c074cdb2b4cb7991b3112
ashish <ashish.mlr.123@gmail.com>
2017-11-08 23:18:09 +0530

    changing port for smtp

--- djangobackend/settings.py
+++ djangobackend/settings.py
@@ -200,7 +200,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 25
 EMAIL_HOST_USER = 'infinv.investments@gmail.com'
 EMAIL_HOST_PASSWORD = 'infinv123'
 
