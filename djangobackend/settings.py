cb58f10558214f9c821cfccec0f876e0a91419d3
ashish <ashish.mlr.123@gmail.com>
2017-11-08 23:11:24 +0530

    testing restore

--- djangobackend/settings.py
+++ djangobackend/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
-    'rest_framework_jwt',
+    # 'rest_framework_jwt',
     'rest_framework.authtoken',
     'djoser',
     'stockapi',
